f71add6624f89b7f3307b82fcf3006cbf6a4b586
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-06-26 18:11:25 -0700

    sum of pisitives (8ku)

---
+++ sum_of_positives.js
@@ -0,0 +1,19 @@
+// Description:
+//
+// You get an array of numbers, return the sum of all of the positives ones.
+//
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//
+// Note: array may be empty, in this case return 0.
+
+//Solution:
+
+function positiveSum(arr) {
+  var total = 0;
+  arr.forEach(function(num){
+  if (num > 0) {
+    total = total + num;
+    }
+   })
+   return total;
+}
