d2f2e70c9163ac84f76445d6558c858350a30244
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-06-26 18:15:13 -0700

    completes drink about (8kyu)

---
+++ drink_about.js
@@ -0,0 +1,32 @@
+// Description:
+//
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+//
+// Rules:
+//
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples:
+//
+// peopleWithAgeDrink(13) === "drink toddy"
+// peopleWithAgeDrink(17) === "drink coke"
+// peopleWithAgeDrink(18) === "drink beer"
+// peopleWithAgeDrink(20) === "drink beer"
+// peopleWithAgeDrink(30) === "drink whisky"
+
+var peopleWithAgeDrink = function(old) {
+  if ( old < 14 ){
+    return 'drink toddy'
+  } else if (old >= 14 && old < 18 ) {
+    return 'drink coke'
+  } else if (old < 21 && old >= 18 ) {
+    return 'drink beer'
+   } else
+  return 'drink whisky';
+};
