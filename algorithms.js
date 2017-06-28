46f5f2156ecff4410e6c1d99a51e2a93e061e9f9
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-06-28 15:01:03 -0700

    adds logging for total guesses and tests (binary search)

--- algorithms.js
+++ algorithms.js
@@ -28,8 +28,43 @@ function binarySearch (array, targetValue) {
        } else if(array[guess] < targetValue){
            min = guess + 1;
        } else {
-          max = guess - 1;  
+          max = guess - 1;
        }
    }
 	return -1;
 };
+
+
+// With logging for total guesses and tests;
+function binarySearch(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
+    var guess;
+    var totalGuess = 0;
+
+   while (min <= max) {
+       guess = Math.floor((min + max) /2);
+       println(guess);
+       totalGuess++;
+       if(array[guess] === targetValue){
+           println(totalGuess);
+           return guess;
+       } else if(array[guess] < targetValue){
+           min = guess + 1;
+       } else {
+            max = guess - 1;
+       }
+   }
+	return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = binarySearch(primes, 73);
+
+println("Found prime at index " + result);
+
+Program.assertEqual(doSearch(primes, 73), 20);
+Program.assertEqual(doSearch(primes, 83), 22);
+Program.assertEqual(doSearch(primes, 11), 4);
