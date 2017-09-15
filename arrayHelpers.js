4e4f729786e11ee5bc5ba642286e5af988896d2c
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-09-15 11:35:58 -0700

    adds best practices refactor

--- arrayHelpers.js
+++ arrayHelpers.js
@@ -77,3 +77,25 @@ if (!Array.prototype.odd) {
     return oddArr;
   };
 }
+
+// Best practices refactor
+Object.assign(Array.prototype, {
+  square() {
+    return this.map(n => n * n);
+  },
+  cube() {
+    return this.map(n => Math.pow(n, 3));
+  },
+  sum() {
+    return this.reduce((p, n) => p + n, 0);
+  },
+  average() {
+    return this.reduce((p, n) => p + n, 0) / this.length;
+  },
+  even() {
+    return this.filter(n => !(n % 2));
+  },
+  odd() {
+    return this.filter(n => n % 2);
+  }
+});
